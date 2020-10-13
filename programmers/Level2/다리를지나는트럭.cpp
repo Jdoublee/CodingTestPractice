db77e270117e053aaafb59e986e6da5a59d2cc85
Jdoublee <hyewon3429@gmail.com>
2020-10-14 01:34:36 +0900

    수정수정

--- programmers/Level2/다리를지나는트럭.cpp
+++ programmers/Level2/다리를지나는트럭.cpp
@@ -1,4 +1,5 @@
 // t를 0부터 시작하는걸로 고치고싶다.
+// 고치긴 했지만 흠냐뤼,,,
 #include <string>
 #include <vector>
 #include <queue>
@@ -24,4 +25,19 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
         }
     }
     return t;
+}
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int t=0,i=0; // t : 0부터
+    queue<pair<int,int>> q;
+    q.push(make_pair(truck_weights[i++],bridge_length)); // 다리 지나는 시간을 저장
+    while(!q.empty()){
+        t++;
+        if(q.front().second==t)
+            q.pop();
+        if(i<truck_weights.size()&&sum(q)+truck_weights[i]<=weight){
+            q.push(make_pair(truck_weights[i++],t+bridge_length));
+        }
+    }
+    return ++t;
 }
