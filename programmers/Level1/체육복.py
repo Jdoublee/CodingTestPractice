9a6747d467b15b022fba8321ff4f413ea83e7187
Jdoublee <hyewon3429@gmail.com>
2020-11-17 22:11:52 +0900

    체육복 수정 완

--- programmers/Level1/체육복.py
+++ programmers/Level1/체육복.py
@@ -11,8 +11,9 @@ def solution(n, lost, reserve):
             cnt += 1
         elif r+1 in lst:
             cnt += 1
-    if cnt > n: # 7번 테케. 왜인지 모르겠지만 n보다 큰 값이 cnt되는 경우 존재 -> n으로 변경
-        cnt = n
-    answer = cnt
+            lst.remove(r+1) # 앞뒤로 체육복 빌려준 경우 -> cnt > n 경우 발생. 아예 lost 리스트에서 제거함으로써 방지.
+    # if cnt > n: # 7번 테케. 왜인지 모르겠지만 n보다 큰 값이 cnt되는 경우 존재 -> n으로 변경
+    #     cnt = n
+    # answer = cnt
     
     return answer
