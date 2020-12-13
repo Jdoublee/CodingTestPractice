7522c39f1eee4d7df6dbcdc1f1cf332dacfda079
Jdoublee <hyewon3429@gmail.com>
2020-12-13 23:41:50 +0900

    pct_최단경로2

--- python-for-coding-test/ch09/2_개선된다익스트라.py
+++ python-for-coding-test/ch09/2_개선된다익스트라.py
@@ -1,4 +1,5 @@
 import sys
+import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 
@@ -8,8 +9,6 @@ n, m = map(int, input().split())
 start = int(input())
 # 각 노드에 연결된 노드 정보
 graph = [[] for i in range(n+1)]
-# 방문 여부 체크
-visited = [False] * (n+1)
 # 최단 거리 테이블 무한대 값으로 초기화
 distance = [INF] * (n+1)
 
@@ -19,33 +18,28 @@ for _ in range(m) :
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
 
-# 방문하지 않은 노드 중 최단 거리 가장 짧은 노드 번호 반환
-def get_smallest_node() :
-    min_value = INF
-    idx = 0
-    for i in range(1, n+1) :
-        if distance[i] < min_value and not visited[i] :
-            min_value = distance[i]
-            idx = i
-    return idx
+# get_smallest_node() 정의 안 함
 
 def dijkstra(start) :
+    q = []
+    # 시작 노드 최단 경로 0 설정, 큐에 삽입
+    heapq.heappush(q, (0, start)) # (거리, 노드)
     distance[start] = 0
-    visited[start] = True
-    for j in graph[start] :
-        distance[j[0]] = j[1]
-    # 전체 노드 반복
-    for i in range(n-1) :
-        # 현재 최단 거리 가장 짧은 노드 찾아서 방문
-        now = get_smallest_node()
-        visited[now] = True
-        # 현재 노드와 연결된 노드들 확인
-        for j in graph[now] :
-            cost = distance[now] + j[1]
-            # 현재 노드 거쳐 이동하는게 더 짧으면 값 바꿔줌
-            if cost < distance[j[0]] :
-                distance[j[0]] = cost
 
+    while q :
+        # 최단 거리 가장 짧은 노드 꺼내기
+        dist, now = heapq.heappop(q)
+        # 현재 노드가 이미 처리된 적 있으면 무시
+        if distance[now] < dist :
+            continue
+        # 현재 노드와 연결된 노드들 확인
+        for i in graph[now] :
+            cost = dist + i[1]
+            # 현재 노드 거쳐 이동하는게 더 짧으면 값 바꿔줌
+            if cost < distance[i[0]] :
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+    
 dijkstra(start)
 
 for i in range(1, n+1) :
@@ -54,4 +48,4 @@ for i in range(1, n+1) :
     else :
         print(distance[i])
 
-# O(V^2)
+# O(ElogV)
