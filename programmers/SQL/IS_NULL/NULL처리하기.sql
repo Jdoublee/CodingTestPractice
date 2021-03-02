e2976f787064958a5ac365af656d56fd1aa09f6e
Jdoublee <hyewon3429@gmail.com>
2021-03-02 10:24:09 +0900

    프로그래머스 sql is null

---
+++ programmers/SQL/IS_NULL/NULL처리하기.sql
@@ -0,0 +1,5 @@
+-- 동물의 생물 종, 이름, 성별 및 중성화 여부를 아이디 순으로 조회하는 SQL문을 작성해주세요.
+-- 이때 프로그래밍을 모르는 사람들은 NULL이라는 기호를 모르기 때문에, 이름이 없는 동물의 이름은 "No name"으로 표시해 주세요.
+
+SELECT ANIMAL_TYPE, IFNULL(NAME, 'No name'), SEX_UPON_INTAKE FROM ANIMAL_INS ORDER BY ANIMAL_ID;
+-- MySQL : IFNULL, Oracle : NVL, SQL server : ISNULL
