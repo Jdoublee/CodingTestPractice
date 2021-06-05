f62cac77dbb95d16d47e43c2b796dba1f5e4d692
Jdoublee <hyewon3429@gmail.com>
2021-06-05 12:25:07 +0900

    프로그래머스 sql

---
+++ programmers/SQL/기타/헤비유저가소유한장소.sql
@@ -0,0 +1,3 @@
+-- 이 서비스에서는 공간을 둘 이상 등록한 사람을 "헤비 유저"라고 부릅니다. 헤비 유저가 등록한 공간의 정보를 아이디 순으로 조회하는 SQL문을 작성해주세요.
+
+SELECT * FROM PLACES WHERE HOST_ID IN (SELECT HOST_ID FROM PLACES GROUP BY HOST_ID HAVING COUNT(*)>1) ORDER BY ID;
